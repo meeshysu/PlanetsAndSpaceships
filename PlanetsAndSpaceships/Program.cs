ce3f84049cecb92f9c928a63f50c8cc8e860f728
Michelle Beshears <meeshysu@gmail.com>
2019-03-15 17:34:34 -0500

    finally finished this exercise. all planets list and crafts

--- PlanetsAndSpaceships/Program.cs
+++ PlanetsAndSpaceships/Program.cs
@@ -8,17 +8,57 @@ namespace PlanetsAndSpaceships
         static void Main(string[] args)
         {
             List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            List<string> newPlanets = new List<string>() { "Uranus", "Neptune" };
+
             planetList.Add("Jupiter");
             planetList.Add("Saturn");
 
-            List<string> newPlanets = new List<string>() { "Xandar", "Hala" };
-
             planetList.AddRange(newPlanets);
+
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+
+            planetList.Add("Pluto");
+
+            List<string> rockyPlanets = planetList.GetRange(0, 4);
+
+            var spaceCrafts = new Dictionary<string, List<string>>();
+            spaceCrafts.Add("Mars", new List<string> { "Surveyor", "Phoenix", "Mariner #3-9", "Viking"});
+            spaceCrafts.Add("Mercury", new List<string> { "Mariner 10" });
+            spaceCrafts.Add("Venus", new List<string> { "Mariner 2", "Soviet Venera 4", "Vega 1 and 2", "Venus Express" });
+            spaceCrafts.Add("Earth", new List<string> { "Humans..." });
+            spaceCrafts.Add("Uranus", new List<string> { "Voyager 2" });
+            spaceCrafts.Add("Neptune", new List<string> { "Voyager 2" });
+            spaceCrafts.Add("Jupiter", new List<string> { "Pioneer 11 and 12", "Voyagers I and II" });
+            spaceCrafts.Add("Saturn", new List<string> { "Pioneer 11", "Voyager 1 and 2" });
+
+
+            planetList.Remove("Pluto");
 
             foreach (var planet in planetList)
             {
-                Console.WriteLine(planet);
+                string spaceCraftString = "";
+                foreach (var (planets, crafts) in spaceCrafts)
+                {
+                    if (planet == planets)
+                    {
+                        foreach (var craft in crafts)
+                        {
+                            spaceCraftString += " " + $"{craft}" + ",";
+                        }
+                    }
+
+                }
+                Console.WriteLine($"{planet}: {spaceCraftString.TrimEnd(',')}");
+
             }
+
+            string rockyPlanetString = "";
+            foreach( var planet in rockyPlanets)
+            {
+                rockyPlanetString += " " + $"{planet}" + ",";
+            }
+            Console.WriteLine($"The Rocky Planets Are: {rockyPlanetString.TrimEnd(',')}");
             Console.ReadLine();
         }
     }
